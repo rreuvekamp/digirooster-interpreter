a8d614bb5701ca4a3d76bc289f96be6618fe6a6d
remi <remi@de180811>
2018-11-19 09:19:22 +0100

    Add buttons to go to the previous and next week
    Also:
     - Arrow left/right too

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ Date.prototype.getWeek = function() {
 	return Math.ceil((((this - firstDay) / 86400000) + firstDay.getDay() + 1) / 7)
 }
 
-function goToCurrentWeek() {
-	let weekNumber = (new Date()).getWeek()
-	location.href = "#week_" + weekNumber
+function goToWeek(week) {
+	console.log(week)
+	if (!week) {
+		week = (new Date()).getWeek()
+	}
+	location.href = "#week_" + week
 }
 
-document.querySelector("header a").addEventListener("click", function(e) {
-	goToCurrentWeek()
-	e.preventDefault()
-})
 
-goToCurrentWeek()
+goToWeek()
 
 function highlightCurrent() {
 	let d = (new Date())
@@ -22,7 +21,6 @@ function highlightCurrent() {
 		m = "0" + m
 	}
 	now = d.getHours() + "" + m
-	console.log(now)
 
 	let week = document.querySelector("section[data-week='" + d.getWeek() + "']")
 
@@ -30,11 +28,12 @@ function highlightCurrent() {
 	end = week.getAttribute("data-endat")
 
 	pixels = (now-offset)/2
-	console.log(offset, now)
 
-	let activities = week.querySelector("div[data-day='" + d.getDay() + "'] div.activities")
+	console.log(d.getDay(), parseInt(d.getDay()), d.getDay()-1)
+	let activities = week.querySelector("div[data-day='" + (d.getDay()-1) + "'] div.activities")
+	console.log(activities)
 
-	if (pixels >= 0 && end-now > 0) {
+	if (activities && pixels >= 0 && end-now > 0) {
 		let line = document.createElement("div")
 		line.classList.add("currenttimeline")
 		line.style.marginTop = pixels + "px"
@@ -51,6 +50,93 @@ for (let i = 0; i < titleEls.length; ++i) {
 		titleEls[i].innerHTML = title
 	})
 }
+
+function determineDisplayedWeek() {
+	let closest = 0;
+	let distance = 0;
+
+	let scroll = document.querySelector("html").scrollTop
+	console.log(scroll)
+
+	let els = document.querySelectorAll("section[data-week]")
+	for (let i = 0; i < els.length; i++) {
+		let diff = scroll-els[i].offsetTop
+		if (diff < 0) {
+			diff *= -1
+		}
+
+		if (distance == 0 || diff < distance) {
+			distance = diff
+			closest = parseInt(els[i].getAttribute("data-week"))
+		}
+	}
+	console.log(closest)
+	return closest
+}
+determineDisplayedWeek()
+
+let displayedWeek = (new Date()).getWeek()
+document.querySelector("button#left").addEventListener("click", function() {
+	prevWeek()
+})
+
+function prevWeek() {
+	let cur = determineDisplayedWeek()
+
+	let els = document.querySelectorAll("section[data-week]")
+	let last = -1;
+	for (let i = 0; i < els.length; i++) {
+		let thiss = els[i].getAttribute("data-week")
+		console.log(thiss, cur, last)
+		if (thiss == cur && last >= 0) {
+			goToWeek(last)
+			return
+		}
+
+		last = thiss
+	}
+
+	// Go to last if we're at the first
+	goToWeek(els[els.length-1].getAttribute("data-week"))
+}
+document.querySelector("button#right").addEventListener("click", function() {
+	nextWeek()
+})
+
+function nextWeek(){
+	let cur = determineDisplayedWeek()
+
+	let els = document.querySelectorAll("section[data-week]")
+	let last = 0;
+	for (let i = 0; i < els.length; i++) {
+		let thiss = els[i].getAttribute("data-week")
+		if (last == cur) {
+			goToWeek(thiss)
+			return
+		}
+
+		last = thiss
+	}
+
+	// Go to first if we're al the last
+	goToWeek(els[0].getAttribute("data-week"))
+}
+document.querySelector("button#currentweek").addEventListener("click", function(e) {
+	goToWeek()
+	e.preventDefault()
+})
+document.addEventListener("keydown", function(e) {
+	console.log(e.keyCode)
+	switch (e.keyCode) {
+	case 37:
+		prevWeek()
+		break
+	case 39:
+		nextWeek()
+		break
+	}
+})
+
 
 highlightCurrent()
 
